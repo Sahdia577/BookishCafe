b8ae03e874cb7bb92a27effb3f3441c7b9bc6ab5
Sahdia Khalid <sahdia@live.nl>
2023-07-22 22:53:55 +0200

    Replaced string value with component value

--- my-first-app/src/components/DrinkButtons.jsx
+++ my-first-app/src/components/DrinkButtons.jsx
@@ -1,12 +1,12 @@
 import './DrinkButtons.css'
 
-export const DrinkButtons = () => {
+export const DrinkButtons = ( { drinkOne, drinkTwo } ) => {
     return (
         <>
             <h1>Would you like tea or coffee?</h1>
             <div className="button-group">
-                <button className="button">Tea</button>
-                <button className="button">Coffee</button>
+                <button className="button">{drinkOne}</button>
+                <button className="button">{drinkTwo}</button>
             </div>
         </>
     );
