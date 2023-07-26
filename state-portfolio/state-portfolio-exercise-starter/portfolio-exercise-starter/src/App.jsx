de135e6cc144392e47ae87756ff79a3a25ae3a6c
Sahdia Khalid <sahdia@live.nl>
2023-07-27 00:04:42 +0200

    Update App.jsx

---
+++ state-portfolio/state-portfolio-exercise-starter/portfolio-exercise-starter/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './App.css';
+import { portfolioItems } from './utils/data';
+import { PortfolioItemPage } from './pages/PortfolioItemPage';
+import { PortfolioPage } from './pages/PortfolioPage';
+
+export const App = () => {
+  console.log(portfolioItems); 
+  const [selectedItem, setSelectedItem] = useState(portfolioItems[7]);
+
+  return <div className="App">
+    {selectedItem ? (
+      <PortfolioItemPage item={selectedItem} />
+    ) : (
+        <PortfolioPage />
+    )}
+  </div>;
+};
