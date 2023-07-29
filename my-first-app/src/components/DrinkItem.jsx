9c9cd6e764c9820332d46e01feb5cdc4fb2e6fe4
Sahdia Khalid <sahdia@live.nl>
2023-07-29 23:01:04 +0200

    Create <DrinkItem />

---
+++ my-first-app/src/components/DrinkItem.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import './DrinkItem.css';
+
+export const DrinkItem = ({ drink }) => {
+    return (
+        <div className ="drink-item">
+            <img src={drink.imgUrl} width={50} height={50} alt={drink.alt} />
+            <p>{drink.name}</p>
+        </div>
+    );
+};
+
