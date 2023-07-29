e7a040436665e0fd14c8e124ca7d7de8d1bc2601
Sahdia Khalid <sahdia@live.nl>
2023-07-29 22:13:07 +0200

    Delete

--- portfolio/portfolio-exercise-starter/portfolio-exercise-starter/src/pages/PortfolioItemPage.jsx
+++
@@ -1,9 +0,0 @@
-export const PortfolioItemPage = ({ item }) => {
-    return (
-        <>
-            <p>{item.title}</p>
-            <img src={item.imageUrl} width="150" height="100" />
-            <p>{item.description}</p>
-        </>
-    ); 
-};
