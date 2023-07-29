8f842f580c6aa2154275a8743117efc47212bef9
Sahdia Khalid <sahdia@live.nl>
2023-07-29 23:13:56 +0200

    Update DrinkList

--- my-first-app/src/components/DrinkList.jsx
+++ my-first-app/src/components/DrinkList.jsx
@@ -1,12 +1,16 @@
-/* eslint-disable react/prop-types */
 import { DrinkItem } from './DrinkItem';
+import PropTypes from 'prop-types'; 
 
-export const DrinkList = ({ drinks }) => { 
+export const DrinkList = ({ drinks }) => {
     return (
-        <>
-            {drinks.map = ((drink) => (
-                <DrinkItem key={drink.id} drink={drink} />
-            ))}  
-        </>
+      <>
+        {drinks.map((drink) => (
+          <DrinkItem key={drink.id} drink={drink} />
+        ))}
+      </>
     );
+  };
+
+DrinkList.propTypes = {
+    drinks: PropTypes.object     
 };
