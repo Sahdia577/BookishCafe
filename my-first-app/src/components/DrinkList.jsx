b977d03f2aae353766bc56c5b2c3b2a5f27202b7
Sahdia Khalid <sahdia@live.nl>
2023-07-29 23:01:23 +0200

    Create DrinkList

---
+++ my-first-app/src/components/DrinkList.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import { DrinkItem } from './DrinkItem';
+
+export const DrinkList = ({ drinks }) => { 
+    return (
+        <>
+            {drinks.map = ((drink) => (
+                <DrinkItem key={drink.id} drink={drink} />
+            ))}  
+        </>
+    );
+};
