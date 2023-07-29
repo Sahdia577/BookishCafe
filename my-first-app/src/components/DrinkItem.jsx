e5887f8435f7ad459ae05b4e74ecd4aa7c16a977
Sahdia Khalid <sahdia@live.nl>
2023-07-29 23:13:42 +0200

    Update DrinkItem

--- my-first-app/src/components/DrinkItem.jsx
+++ my-first-app/src/components/DrinkItem.jsx
@@ -1,12 +1,15 @@
-/* eslint-disable react/prop-types */
 import './DrinkItem.css';
+import PropTypes from 'prop-types'; 
 
 export const DrinkItem = ({ drink }) => {
-    return (
-        <div className ="drink-item">
-            <img src={drink.imgUrl} width={50} height={50} alt={drink.alt} />
-            <p>{drink.name}</p>
-        </div>
-    );
+	return (
+		<div className="drink-item">
+			<img src={drink.imgUrl} width={50} height={50} alt={drink.alt} />
+			<p>{drink.name}</p>
+		</div>
+	);
 };
 
+DrinkItem.propTypes = {
+    drink: PropTypes.object
+};
