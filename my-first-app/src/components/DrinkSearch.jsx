0bbbac19249e7215aad61adadcd69020376d4886
Sahdia Khalid <sahdia@live.nl>
2023-07-29 23:19:12 +0200

    Update DrinkSearch

--- my-first-app/src/components/DrinkSearch.jsx
+++ my-first-app/src/components/DrinkSearch.jsx
@@ -1,16 +1,16 @@
+import { TextInput } from './UI/TextInput';
 import { useState } from 'react';
-import { availableDrinks } from '../utils/data';
 import { DrinkList } from './DrinkList';
-import { TextInput } from './ui/TextInput';
+import { availableDrinks } from '../utils/data';
 
 export const DrinkSearch = () => {
-  const [searchField, setSearchField] = useState('test drink');
-
-  return (
-    <>
-      <label>Search for drinks:</label>
-      <TextInput />
-      <DrinkList drinks={availableDrinks} />
-    </>
-  );
+    const [searchField, setSearchField] = useState('test drink');
+    
+    return (
+        <>
+            <label><b>Search for your drink:</b></label>
+            <TextInput />
+            <DrinkList drinks={availableDrinks} />
+        </>       
+    );
 };
