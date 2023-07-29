8e2c248c0bcc1e10192f5c2f2656ccca8433b5a6
Sahdia Khalid <sahdia@live.nl>
2023-07-29 23:00:42 +0200

    Add <DrinkList /> and { availableDrinks }

--- my-first-app/src/components/DrinkSearch.jsx
+++ my-first-app/src/components/DrinkSearch.jsx
@@ -1,13 +1,16 @@
 import { TextInput } from './UI/TextInput';
 import { useState } from 'react';
+import { DrinkList } from './DrinkList';
+import { availableDrinks } from '../utils/data';
 
 export const DrinkSearch = () => {
-    const [searchField, setSearchField] = useState();
+    const [searchField, setSearchField] = useState('test drink');
+    
     return (
         <>
-            <label><b>Search for your drink:</b> </label>
+            <label><b>Search for your drink:</b></label>
             <TextInput />
-            <p>{searchField}</p>                     
+            <DrinkList drinks={availableDrinks} />
         </>       
     );
 };
