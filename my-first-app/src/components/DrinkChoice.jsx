30ce191cc336c77eacd8dc62901fa557ec3bd5c4
Sahdia Khalid <sahdia@live.nl>
2023-07-30 20:51:22 +0200

    Add <Button />

--- my-first-app/src/components/DrinkChoice.jsx
+++ my-first-app/src/components/DrinkChoice.jsx
@@ -1,16 +1,18 @@
 import PropTypes from 'prop-types';
+import { Button } from './UI/Button';
 
-export const DrinkChoice = ({ drink }) => {
+export const DrinkChoice = ({ drink, clickFn }) => {
     return (
       <>
-            <p>{drink.name}</p>
-            <img src={drink.imgUrl} alt={drink.alt} width="100px" height="100px" />
-            <p>You drink will be ready in a few minutes!</p>
+        <h2>Your choice: {drink.name}</h2>
+        <img src={drink.imgUrl} alt={drink.alt} width="100px" height="100px" />
+        <h3>You drink will be ready in a few minutes!</h3>
+        <Button text={'Change selection'}  clickFn={() => clickFn()} />        
       </>
     );
 };
 
 DrinkChoice.propTypes = {
-  drink: PropTypes.object, 
-
+  drink: PropTypes.object,
+  clickFn: PropTypes.func
 };
