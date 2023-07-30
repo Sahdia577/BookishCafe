7b300167f274f365c59a2fed0bbb89b17aaab507
Sahdia Khalid <sahdia@live.nl>
2023-07-30 16:21:58 +0200

    Add clickFn and PropTypes.func

--- my-first-app/src/components/DrinkList.jsx
+++ my-first-app/src/components/DrinkList.jsx
@@ -1,16 +1,17 @@
 import { DrinkItem } from './DrinkItem';
 import PropTypes from 'prop-types'; 
 
-export const DrinkList = ({ drinks }) => {
+export const DrinkList = ({ drinks, clickFn }) => {
     return (
       <>
         {drinks.map((drink) => (
-          <DrinkItem key={drink.id} drink={drink} />
+          <DrinkItem key={drink.id} drink={drink} clickFn={clickFn} />
         ))}
       </>
     );
   };
 
 DrinkList.propTypes = {
-    drinks: PropTypes.object     
+  drinks: PropTypes.object,
+  clickFn: PropTypes.func
 };
