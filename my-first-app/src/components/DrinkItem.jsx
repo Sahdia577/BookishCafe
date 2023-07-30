f447878eb392901309c62a1b45a31c58d4856409
Sahdia Khalid <sahdia@live.nl>
2023-07-30 16:22:21 +0200

    Add clickFn and PropTypes.func

--- my-first-app/src/components/DrinkItem.jsx
+++ my-first-app/src/components/DrinkItem.jsx
@@ -1,15 +1,16 @@
 import './DrinkItem.css';
 import PropTypes from 'prop-types'; 
 
-export const DrinkItem = ({ drink }) => {
+export const DrinkItem = ({ drink, clickFn }) => {
 	return (
-		<div className="drink-item">
+		<button className="drink-item" onClick={() => clickFn(drink)}>
 			<img src={drink.imgUrl} width={50} height={50} alt={drink.alt} />
 			<p>{drink.name}</p>
-		</div>
+		</button>
 	);
 };
 
 DrinkItem.propTypes = {
-    drink: PropTypes.object
+	drink: PropTypes.object,
+	clickFn: PropTypes.func
 };
