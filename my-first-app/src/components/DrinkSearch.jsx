583be7009a0813de0adf8bd23a2bec9d74be81e0
Sahdia Khalid <sahdia@live.nl>
2023-07-31 21:58:26 +0200

    Create handleChange and matchedDrinks.

--- my-first-app/src/components/DrinkSearch.jsx
+++ my-first-app/src/components/DrinkSearch.jsx
@@ -5,13 +5,21 @@ import { availableDrinks } from '../utils/data';
 import PropTypes from 'prop-types';
 
 export const DrinkSearch = ({ clickFn }) => {
-    const [searchField, setSearchField] = useState('test drink');
+    const [searchField, setSearchField] = useState('');
+
+    const handleChange = (event) => {
+        setSearchField(event.target.value);
+    };
+
+    const matchedDrinks = availableDrinks.filter((drink) => {
+        return drink.name.toLowerCase().includes(searchField.toLowerCase());
+    });
     
     return (
         <>
             <label><h2>Search for your drink:</h2></label>
-            <TextInput />
-            <DrinkList drinks={availableDrinks} clickFn={clickFn} />
+            <TextInput changeFn={handleChange} />
+            <DrinkList drinks={matchedDrinks} clickFn={clickFn} />
         </>       
     );
 };
