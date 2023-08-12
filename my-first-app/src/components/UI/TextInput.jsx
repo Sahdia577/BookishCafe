d599e42c0d963ec467b80e4d2138a1d7f2084762
Sahdia Khalid <sahdia@live.nl>
2023-08-12 22:57:59 +0200

    Update styling

--- my-first-app/src/components/UI/TextInput.jsx
+++ my-first-app/src/components/UI/TextInput.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { Input } from '@chakra-ui/react'
 
 export const TextInput = ({ onChange, ...props }) => {
-     <Input onChange={onChange} variant='flushed' {...props} />
+     <Input variant='flushed' onChange={onChange} {...props} />
 };
 
 
