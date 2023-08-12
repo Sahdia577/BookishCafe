7c788e3100c368d3aa2336066e767b05f64fa1c0
Sahdia Khalid <sahdia@live.nl>
2023-08-12 22:40:17 +0200

    Add WrapItem and onClick

--- my-first-app/src/components/DrinkItem.jsx
+++ my-first-app/src/components/DrinkItem.jsx
@@ -1,16 +1,23 @@
 import PropTypes from 'prop-types'; 
-import { Center, Image, Text } from '@chakra-ui/react'
+import { Center, Image, Text, WrapItem } from '@chakra-ui/react'
 
-export const DrinkItem = ({ drink, clickFn }) => {
+export const DrinkItem = ({ drink, onClick }) => {
 	return (
-		<Center onClick={() => clickFn(drink)} cursor='pointer' gap={8} my={3}>
-			<Image src={drink.imgUrl} width={50} height={50} alt={drink.alt} />
+		<WrapItem>
+		<Center onClick={() => onClick(drink)} cursor='pointer' gap={8} my={3}>
+				<Image
+					src={drink.imgUrl}
+					width={50}
+					height={50}
+					alt={drink.alt}
+				/>
 			<Text fontSize='1.2rem' fontWeight={'400'}>{drink.name}</Text>
-		</Center>
+			</Center>
+		</WrapItem>
 	);
 };
 
 DrinkItem.propTypes = {
 	drink: PropTypes.object,
-	clickFn: PropTypes.func
+	onClick: PropTypes.func
 };
