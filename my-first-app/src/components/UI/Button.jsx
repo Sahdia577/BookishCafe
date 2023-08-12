cae7ed26435c806ea447bb797bc7335c73b35020
Sahdia Khalid <sahdia@live.nl>
2023-08-12 21:31:39 +0200

    Add {props.children}

--- my-first-app/src/components/UI/Button.jsx
+++ my-first-app/src/components/UI/Button.jsx
@@ -2,13 +2,7 @@ import PropTypes from 'prop-types';
 import { Button as CButton } from '@chakra-ui/react'
 
 export const Button = ({ clickFn, ...props }) => (
-    <CButton
-        onClick={clickFn}
-        w={200}
-        bg='green.600'
-        color='white'
-    >
-        Change selection
+    <CButton onClick={clickFn} {...props}>
         {props.children}
     </CButton>  
 );
